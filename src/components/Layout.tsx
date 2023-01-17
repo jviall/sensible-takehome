2e64fd049958170a16862ceb1d9f590f448f174f
James Viall <jamesviall@gmail.com>
2023-01-17 09:32:02 +0000

    Adds support for click-to-call, improves responsive layout.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,8 @@ export default function Layout({ header, main }: Props) {
       id="container"
       className={`
         h-full w-full grid justify-items-center items-start
-        grid-cols-[1fr_minmax(360px,2fr)_1fr]
+        grid-cols-[min-content_1fr_min-content]
+        lg:grid-cols-[1fr_4fr_1fr]
         grid-rows-[5rem_1fr_1.5rem]
         h-[100vh]
         `}
@@ -22,8 +23,8 @@ export default function Layout({ header, main }: Props) {
         {main}
       </div>
       {/* content borders */}
-      <div className="h-full col-start-1 border-r-[1px] border-slate-300 dark:border-zinc-700 row-start-1 row-span-3 justify-self-end" />
-      <div className="h-full col-start-3 border-l-[1px] border-slate-300 dark:border-zinc-700 row-start-1 row-span-3 justify-self-start" />
+      <div className="hidden lg:block h-full col-start-1 border-r-[1px] border-slate-300 dark:border-zinc-700 row-start-1 row-span-3 justify-self-end" />
+      <div className="hidden lg:block h-full col-start-3 border-l-[1px] border-slate-300 dark:border-zinc-700 row-start-1 row-span-3 justify-self-start" />
     </div>
   );
 }
